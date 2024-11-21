5f4fb76e4d594f09b835296320f7c0c1e95679f2
Hesham Adel <hesham.adel@pickspace.com>
2024-11-21 02:57:51 +0200

    adds dockerfiles
    adds dockerfile and docker compose

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
 
 function App() {
+  const StyledApp = styled.div`
+    background-image: url('/space_background.svg');
+    background-size: cover;
+    background-repeat: no-repeat;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+    font-family: Arial, sans-serif;
+  `;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <StyledApp>
+      <h1 className="text-3xl font-bold underline">
+      </h1>
+    </StyledApp>
+  )
 }
 
 export default App;
